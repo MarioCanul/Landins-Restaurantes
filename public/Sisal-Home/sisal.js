896887711630b9550091e07af052ca7fde563c4a
MarioCanul <mario_can12@hotmail.com>
2021-09-02 08:52:52 -0500

    resubido a mi repositorio

---
+++ public/Sisal-Home/sisal.js
@@ -0,0 +1,167 @@
+// Menu header
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  // end header menu
+  //   back to top
+  $('#backTotop').click(function(){ 
+    $("html, body").animate({ scrollTop: 0 }, 600); 
+    return false; 
+});
+// back to top
+
+  $('.comments-sisal').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+$('.Galery').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:false,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:3
+      }
+  }
+})
+$('.Galery-desktop').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:1
+      },
+      1000:{
+          items:1
+      }
+  }
+})
+$('.Sister-Carrousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:false,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
+
+var sliderlight = $(".modal-carrousel").owlCarousel({
+    items: 1,
+    center: true,
+    autoHeight: true,
+    autoHeightClass: "owl-height",
+    margin: 10,
+    loop: true,
+
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1,
+  
+        }
+    },
+    navText: [
+      '<span class="material-icons"> arrow_back_ios </span>',
+      '<span class="material-icons"> arrow_forward_ios </span>',
+    ],
+    nav: false,
+    items: 1,
+    responsiveClass: true,
+    singleItem: true,
+  });
+$(".itemLightBox").on("click", function () {
+    let idItemGallery = $(this).data("lightboxid");
+    console.log(idItemGallery);
+    sliderlight.trigger("to.owl.carousel", [idItemGallery, 100]);
+    setTimeout(() => {
+      $(".modal-bg").addClass("bg-active");
+    }, 300);
+  });
+
+  $(".modal-close").on("click", function (e) {
+    console.log(e.target.id)
+    
+     $(".modal-bg").removeClass("bg-active");
+    
+   });
+
+const linksmobile = document.querySelectorAll(".myLinksNav a");
+  for (const link of linksmobile) {
+    link.addEventListener("click", clickHandler);
+  }
+  function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    if (href=='#ReserveNow') {
+      var anchor = document.createElement('a');    
+anchor.href = "https://www.opentable.com.mx/r/hacienda-sisal-cancun-reservations-cancun?restref=1057438&lang=en-US&ot_source=Restaurant%20website";
+anchor.target="_blank";
+anchor.click();  
+    }
+    const offsetTop = document.querySelector(href).offsetTop;
+  
+    scroll({
+      top: offsetTop,
+      behavior: "smooth"
+    });
+  }
+  const linksdesk = document.querySelectorAll(".myLinks a");
+  for (const link of linksdesk) {
+    link.addEventListener("click", clickHandler);
+  }
+  function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    if (href=='#ReserveNow') {
+      var anchor = document.createElement('a');    
+anchor.href = "https://www.opentable.com.mx/r/hacienda-sisal-cancun-reservations-cancun?restref=1057438&lang=en-US&ot_source=Restaurant%20website";
+anchor.target="_blank";
+anchor.click();  
+    }
+    const offsetTop = document.querySelector(href).offsetTop;
+  
+    scroll({
+      top: offsetTop,
+      behavior: "smooth"
+    });
+  }
---
+++ public/murales/index.js
@@ -0,0 +1,188 @@
+// Murales
+// Menu header
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  // end header menu
+//   back to top
+  $('#backTotop').click(function(){ 
+    $("html, body").animate({ scrollTop: 0 }, 600); 
+    return false; 
+});
+// back to top
+
+// section5
+  $('.comments-trat').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    center:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+// end section5
+
+// section 6
+$('.platillos-grid').owlCarousel({
+  with:10,
+  loop:true,
+  items:3,
+  margin:10,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:3,
+
+      }
+  }
+})
+
+$('.restaurants-desktop').owlCarousel({
+    with:10,
+    loop:true,
+    items:3,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1,
+  
+        }
+    }
+  })
+  var sliderlight = $(".modal-carrousel").owlCarousel({
+    items: 1,
+    center: true,
+    autoHeight: false,
+    autoHeightClass: "owl-height",
+    margin: 10,
+    loop: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1,
+  
+        }
+    },
+    navText: [
+      '<span class="material-icons"> arrow_back_ios </span>',
+      '<span class="material-icons"> arrow_forward_ios </span>',
+    ],
+    nav: false,
+    items: 1,
+    responsiveClass: true,
+    singleItem: true,
+  });
+//   var modalbg = document.querySelector('.modal-bg');
+  $(".itemLightBox").on("click", function () {
+    
+    let idItemGallery = $(this).data("lightboxid");
+    console.log(idItemGallery);
+    sliderlight.trigger("to.owl.carousel", [idItemGallery, 100]);
+    setTimeout(() => {
+      $(".modal-bg").addClass("bg-active");
+    }, 100);
+  });
+ 
+  $(".modal-close").on("click", function (e) {
+   console.log(e.target.id)
+   
+    $(".modal-bg").removeClass("bg-active");
+   
+  });
+// end section 6
+
+// section 7
+$('.Sister-Carrousel').owlCarousel({
+    with:10,
+    loop:true,
+    items:3,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5,
+  
+        }
+    }
+  })
+//  end section 7
+
+const linksmobile = document.querySelectorAll(".myLinksNav a a");
+  for (const link of linksmobile) {
+    link.addEventListener("click", clickHandler);
+  }
+  function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    console.log(href)
+    if (href=='#ReserveNow') {
+      var anchor = document.createElement('a');    
+anchor.href = "https://www.opentable.com.mx/r/los-murales-reservations-cancun?restref=1146829&lang=en-US&ot_source=Restaurant%20website";
+anchor.target="_blank";
+anchor.click();  
+    }
+    const offsetTop = document.querySelector(href).offsetTop;
+  
+    scroll({
+      top: offsetTop,
+      behavior: "smooth"
+    });
+  }
+
+  const links = document.querySelectorAll(".myLinks a");
+console.log(links)
+  for (const link of links) {
+    link.addEventListener("click", clickHandler);
+  }
+  
+  function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    console.log(href);
+    if (href=='#ReserveNow') {
+      var anchor = document.createElement('a');    
+anchor.href = "https://www.opentable.com.mx/r/los-murales-reservations-cancun?restref=1146829&lang=en-US&ot_source=Restaurant%20website";
+anchor.target="_blank";
+anchor.click();  
+    }
+    const offsetTop = document.querySelector(href).offsetTop;
+    scroll({
+      top: offsetTop,
+      behavior: "smooth"
+    });
+  }
